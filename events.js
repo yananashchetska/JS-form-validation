f2bf0a99d2fcaecfc8dea6c0c82e2d8b8aa42ebf
= <=>
2024-06-20 14:19:55 +0300

    prevent default practiced

--- events.js
+++ events.js
@@ -1,12 +1,18 @@
 // Events Delegation
 
 const buttonsContainer = document.querySelector('.buttons');
+const mainLink = document.querySelector('.buttons__link');
 
 document.addEventListener('click', function(ev) {
 
     if (ev.target.matches('.click-me')) {
         console.log('yeah, that`s correct!');
         buttonsContainer.style.backgroundColor = 'green';
+    }
+
+    else if (ev.target.matches('.buttons__link')) {
+        console.log('you clicled on the link');
+        buttonsContainer.style.backgroundColor = 'white';
     }
 
     else {
@@ -16,3 +22,10 @@ document.addEventListener('click', function(ev) {
 
     }
 })
+
+// practicing preventDefault
+
+mainLink.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    console.log(ev);
+});
